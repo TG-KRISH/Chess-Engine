42bebc1564dbe78abc33d7e1eff8972d8d79da6e
Gokul <79579584+TG-KRISH@users.noreply.github.com>
2021-06-18 20:10:10 +0530

    Update test_basic.py

--- test/test_basic.py
+++ test/test_basic.py
@@ -2,7 +2,7 @@ import numpy as np
 
 import bitboard
 
-def test_bitscan():
+def test_bitscan(eg):
     assert bitboard.lsb_bitscan(np.uint64(0xF000000000000000)) == np.uint8(60)
     assert bitboard.msb_bitscan(np.uint64(0xF000000000000000)) == np.uint8(63)
 
