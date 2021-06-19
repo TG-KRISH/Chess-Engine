8acaf78e2ac6b739cf8596fb1f7ea6c720b02a3f
Gokul <79579584+TG-KRISH@users.noreply.github.com>
2021-06-19 16:16:12 +0530

    Update test_basic.py

--- test/test_basic.py
+++ test/test_basic.py
@@ -6,5 +6,5 @@ def test_bitscan():
     assert bitboard.lsb_bitscan(np.uint64(0xF000000000000000)) == np.uint8(60)
     assert bitboard.msb_bitscan(np.uint64(0xF000000000000000)) == np.uint8(63)
 
-def test_popcount():
+ef test_popcount():
     assert bitboard.pop_count(np.uint64(0xF0000F00000F0000)) == np.uint8(12)
